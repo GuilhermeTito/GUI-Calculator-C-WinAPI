3d1088d6edf3a0e6064a70d14ca0cf6f2650870b
GuilhermeTito <guilhermeferrazronha123@gmail.com>
2025-01-07 22:58:05 -0300

    feat: added controls to window

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+void AddControls(HWND hWnd);
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     LPSTR lpCmdLine, int nCmdShow)
 {
@@ -33,19 +35,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
     AdjustWindowRect(&window_rect, kMainWindowStyle, FALSE);
 
-    HWND main_window = CreateWindowW(
-        kClassName,
-        L"Calculator",
-        kMainWindowStyle,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
+    HWND main_window = CreateWindowW(kClassName, L"Calculator",
+        kMainWindowStyle, CW_USEDEFAULT, CW_USEDEFAULT,
         window_rect.right - window_rect.left,
-        window_rect.bottom - window_rect.top,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
-    );
+        window_rect.bottom - window_rect.top, NULL, NULL, hInstance, NULL);
 
     if (!main_window)
     {
@@ -69,6 +62,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
+        case WM_CREATE:
+            AddControls(hWnd);
+            break;
         case WM_DESTROY:
             PostQuitMessage(0);
             break;
@@ -76,4 +72,77 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             return DefWindowProcW(hWnd, uMsg, wParam, lParam);
     }
     return 0;
+}
+
+void AddControls(HWND hWnd)
+{
+    const DWORD kTextStaticStyle = WS_VISIBLE | WS_CHILD | SS_RIGHT;
+    const DWORD kEditStyle = WS_VISIBLE | WS_CHILD | ES_RIGHT;
+    const DWORD kButtonStyle = WS_VISIBLE | WS_CHILD;
+
+    CreateWindowW(L"STATIC", L"", kTextStaticStyle, 20, 40, 330, 15, hWnd,
+        NULL, (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"EDIT", L"", kEditStyle, 20, 65, 330, 23, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"CE", kButtonStyle, 20, 100, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"C", kButtonStyle, 105, 100, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"<--", kButtonStyle, 190, 100, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"/", kButtonStyle, 275, 100, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"7", kButtonStyle, 20, 135, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"8", kButtonStyle, 105, 135, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"9", kButtonStyle, 190, 135, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"*", kButtonStyle, 275, 135, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"4", kButtonStyle, 20, 170, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"5", kButtonStyle, 105, 170, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"6", kButtonStyle, 190, 170, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"-", kButtonStyle, 275, 170, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"1", kButtonStyle, 20, 205, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"2", kButtonStyle, 105, 205, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"3", kButtonStyle, 190, 205, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"+", kButtonStyle, 275, 205, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"+/-", kButtonStyle, 20, 240, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"0", kButtonStyle, 105, 240, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L",", kButtonStyle, 190, 240, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+
+    CreateWindowW(L"BUTTON", L"=", kButtonStyle, 275, 240, 75, 25, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
 }
